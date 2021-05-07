d8b7269a75d5060665a7de76d8462ee8f919d3fa
ACB <78452262+adambedingfield@users.noreply.github.com>
2021-05-07 01:44:14 -0700

    linked database to server, added ignored account.js  file to hold sql acc/pword so they arent displayed on github

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+const accDetails = require('./account');
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your MySQL username
+        user: accDetails.username,
+        // Your MySQL password
+        password: accDetails.pWord,
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (not found)
 app.use((req, res) => {
